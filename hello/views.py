7f607b8aedec8a89b0e1cf1cc5f037df31cb123a
MilanMalta <113123937+MilanMalta@users.noreply.github.com>
2022-09-13 09:59:14 +0200

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
+import requests
+
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    
+    response = requests.get('http://cas.sk')
+    return HttpResponse(response) 
+    
+    
+    
 
 
 def db(request):
